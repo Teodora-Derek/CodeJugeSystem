19e362f501e6f54d9ed05c325f9ec2485531e34e
Teodora-Derek <teodoraderek@gmail.com>
2023-11-21 17:43:29 +0200

    SomeThing in FileContentIndex changed. I don't know what honestly.

--- CodeJudgeSystemWebApplication/wwwroot/js/frontpage.js
+++ CodeJudgeSystemWebApplication/wwwroot/js/frontpage.js
@@ -1,56 +1,33 @@
-﻿const uri = 'api/students';
-let students = [];
+﻿document.addEventListener('DOMContentLoaded', function () {
+    // Wait for the DOM content to be fully loaded
 
-function getStudents() {
-    fetch(uri)
-        .then(response => response.json())
-        .then(data => _displayStudents(data))
-        .catch(error => console.error('Unable to get students.', error));
-}
+    const form = document.querySelector('form'); // Assuming you have only one form on the page
 
-function _displayCount(studentsCount) {
-    const name = (studentsCount === 1) ? 'student' : 'students';
+    form.addEventListener('submit', function (event) {
+        event.preventDefault(); // Prevent the default form submission
 
-    document.getElementById('counter').innerText = `${studentsCount} ${name}`;
-}
+        // Get form data
+        const formData = new FormData(form);
 
-function _displayStudents(data) {
-    const tBody = document.getElementById('students');
-    tBody.innerHTML = '';
-
-    _displayCount(data.length);
-
-    const button = document.createElement('button');
-
-    data.forEach(item => {
-        let isCompleteCheckbox = document.createElement('input');
-        isCompleteCheckbox.type = 'checkbox';
-        isCompleteCheckbox.disabled = true;
-        isCompleteCheckbox.checked = item.isComplete;
-
-        let editButton = button.cloneNode(false);
-        editButton.innerText = 'Edit';
-        editButton.setAttribute('onclick', `displayEditForm(${item.id})`);
-
-        let deleteButton = button.cloneNode(false);
-        deleteButton.innerText = 'Delete';
-        deleteButton.setAttribute('onclick', `deleteItem(${item.id})`);
-
-        let tr = tBody.insertRow();
-
-        let td1 = tr.insertCell(0);
-        td1.appendChild(isCompleteCheckbox);
-
-        let td2 = tr.insertCell(1);
-        let textNode = document.createTextNode(item.name);
-        td2.appendChild(textNode);
-
-        let td3 = tr.insertCell(2);
-        td3.appendChild(editButton);
-
-        let td4 = tr.insertCell(3);
-        td4.appendChild(deleteButton);
+        // Make a POST request to your ASP.NET Core Web API endpoint
+        fetch('https://your-api-endpoint.com/upload', {
+            method: 'POST',
+            body: formData,
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json(); // Assuming your API returns JSON data
+            })
+            .then(data => {
+                // Handle the response from the server
+                console.log('Server response:', data);
+                // You can update the UI or perform other actions based on the response
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                // Handle errors, such as network issues or server errors
+            });
     });
-
-    todos = data;
-}
+});
