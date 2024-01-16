a3e538d24c9f4a7685d08cb52d4824573ed5cf9c
Atanas Adamov <147752297+AtanasAd@users.noreply.github.com>
2024-01-16 16:12:44 +0200

    Add files via upload

---
+++ CodeJudgeSystemWebApplication/DBModels/AssignmentModel.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+using System;
+
+namespace CodeJudgeSystemWebApplication.Models
+{
+    public class AssignmentModel
+    {
+        [Required]
+        [MemberNotNull(nameof(Subject))]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Subject { get; set; } = string.Empty;
+
+        [Required]
+        [MemberNotNull(nameof(AssignmentName))]
+        [Column(TypeName = "nvarchar(100)")]
+        public string AssignmentName { get; set; } = string.Empty;
+
+        [Required]
+        [MemberNotNull(nameof(DueDate))]
+        [Column(TypeName = "nvarchar(100)")]
+        public string DueDate { get; set; } = string.Empty;
+
+        [Required]
+        public short Course { get; set;}
+
+        [Required]
+        public short Semester { get; set; }
+
+        [Required]      
+        public short TargetGroup { get; set; }
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+    }
+
+}   
